5e5300f6fc353fa42b4fb11654ac99169e323db4
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-06-14 00:47:18 -0300

    faet: handle errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -22,8 +23,14 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       name,
       password,
     })
-  } catch (error) {
-    return reply.status(409).send()
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: err.message,
+      })
+    }
+
+    return reply.status(500).send() // TODO: fix me
   }
 
   return reply.status(201).send()
