7c7622af1f4240e3f61cb36f499b1ede2c09905e
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-06-14 00:06:57 -0300

    feat: hashing password and check if user already exists by email

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import fastify from 'fastify'
 
-import { register } from '@/http/controllers/register'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-app.post('/users', register)
+app.register(appRoutes)
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,9 +1,10 @@
-import { FastifyReply, FastifyRequest } from "fastify"
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 import { prisma } from '@/lib/prisma'
 
-export async function register (request: FastifyRequest, reply: FastifyReply) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
@@ -12,11 +13,23 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
