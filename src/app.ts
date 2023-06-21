4ad552cc18b02fec9d16c9ef4aff62d9800583e7
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-06-21 00:31:37 -0300

    feat: JWT auth and profile controller

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+
+import fastifyJwt from '@fastify/jwt'
 
 import { appRoutes } from './http/routes'
-import { ZodError } from 'zod'
+
 import { env } from './env'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
